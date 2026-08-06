bca5d01a1cd9430005d8359d104f67322ee8fbf5
D1skborste <kossan_90@hotmail.com>
2026-08-06 08:10:39 +0200

    Update Python script reference in run.bat

--- main/run.bat
+++ main/run.bat
@@ -1,6 +1,6 @@
 @echo off
 
 :: Temporary workaround! Point to the python.exe, even if you're not in a virtual env.
-"C:\Users\MAKA6661\AppData\Local\Python\bin\python.exe" "%~dp0compare_times_excel.backup.py" %*
+"C:\Users\PCMasterRace\AppData\Local\Python\bin\python.exe" "%~dp0compare_times.backup.py" %*
 
 pause
